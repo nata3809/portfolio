0b880d5dcd479c1c63a8f8480a850d707990a4bc
Natasja Nielsen <Nata3809@stud.kea.dk>
2023-01-10 22:16:04 +0100

    rettelser

--- script.1.js
+++
@@ -1,8 +0,0 @@
-window.onload = () => {
-  const burger = document.querySelector(".burger");
-  const menu = document.querySelector(".menu");
-
-  burger.addEventListener("mousedown", () => {
-    menu.classList.toggle("active");
-  });
-};
